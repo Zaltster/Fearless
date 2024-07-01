cc3b5370deec37681c449ae627a70fa191b0cc03
Zaltster <sms9386@psu.edu>
2024-07-01 18:03:19 -0400

    please

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
+import ahri from './champs/ahri.png';
+import aatrox from './champs/aatrox.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={ahri} className="App-logo" alt="logo" width={100} height={100} />
+        <img src={aatrox} className="App-logo" alt="logo" width={100} height={100} />
       </header>
     </div>
   );
